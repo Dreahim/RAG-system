9c6c54b03f818920902cccd38d95f7c22a91de41
Dreahim <mohammad.drahim@yahoo.com>
2026-08-31 12:53:34 +0300

    moving application files & folders into src directory

--- main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from dotenv import load_dotenv
-load_dotenv(".env")
-from routes import base, users
-
-app = FastAPI()
-
-app.include_router(base.base_router)
-app.include_router(users.users_router)
