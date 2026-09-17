2d1c1c013b4f9673e3914793212fe6be4c382073
Dreahim <mohammad.drahim@yahoo.com>
2026-09-17 12:37:56 +0300

    fixing a bug (adding the user prompt to the query)

--- src/controllers/NLPController.py
+++ src/controllers/NLPController.py
@@ -109,7 +109,8 @@ class NLPController(BaseController ):
             for idx, doc in enumerate(retrieved_documents)
         ])
 
-        footer_prompt = self.template_parser.get("rag", "footer_prompt")
+        footer_prompt = self.template_parser.get("rag", "footer_prompt",
+                                                 {"query": query})
 
         # step3: Construct generation client prompts
         chat_history = [
